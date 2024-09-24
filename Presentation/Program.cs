7a17989c2b54e244d846e514dc8681bcb3f7c629
e.asheri <e.asheri@Rayanbourse.ir>
2024-09-24 09:17:14 +0330

    cleaning main method

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -5,46 +5,66 @@ using Infrastructure.DataBase.Sql;
 using Infrastructure.DataBase.Sql.Repository;
 using Microsoft.EntityFrameworkCore;
 
-namespace Presentation
+namespace Presentation;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        ConfigServices(builder.Services);
+        ConfigDataBase(builder.Services,builder.Configuration);
+
+        var app = builder.Build();
+
+        ConfigSwagger(app);
+        ConfigMiddleWares(app);
+
+        app.Run();
+    }
+
+    private static void ConfigMiddleWares(WebApplication app)
+    {
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+    }
+
+    private static void ConfigSwagger(WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddScoped<IPersonRepository,PersonRepository>();
-            builder.Services.AddScoped<IPersonService,PersonService>();
-
-            builder.Services.AddDbContext<TamrinDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"));
-            });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+    }
+
+    private static void ConfigServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen();
+
+        IOC(services);
+    }
+
+    private static void IOC(IServiceCollection services)
+    {
+        services.AddScoped<IPersonRepository, PersonRepository>();
+        services.AddScoped<IPersonService, PersonService>();
+    }
+
+    private static void ConfigDataBase(IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddDbContext<TamrinDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DataBase"));
+        });
     }
 }
